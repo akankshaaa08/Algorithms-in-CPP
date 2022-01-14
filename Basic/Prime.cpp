0bf5069c3ca1ec634f4a95da6276129ebefe608f
Akanksha Pandaw <akankshapandaw8@gmail.com>
2022-01-14 23:42:31 +0530

    Checking whether a number is prime or not

--- Basic/Prime.cpp
+++ Basic/Prime.cpp
@@ -1,74 +1,62 @@
-/*
-Optimal Method;
-If a number x divides the given number, then
-there is always a pair of x such that, it will also divide the given number
-The numbers are in two different halves
-Therefore loop will go from 2 to root(number)
-
-//Prime Numbers: 2,3    5,7   11,13    17,19     23,31    37
-Time Complexity: O(root n)
-*/
 #include<iostream>
 using namespace std;
 
-int Bool(int n)
+//-----NAIVE APPROACH----
+//T.C ==> O(n)
+int prime_naive(int num)
 {
-    if(n==1)                        //corner case 1
-		return false;
 
-	if(n==2 || n==3)                //corner case 2
-		return true;
+    int count = 0;
 
-	if(n%2==0 || n%3==0)            //corner case 3
-		return false;
+    if(num==0 || num==1)        // edge case 1
+    {
+        count++;
+    }        
+    
+    for(int i=2 ; i<num ; i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+            break;
+        }    
+    }
 
-	for(int i=5; i*i<=n; i=i+6)
-	{
-		if(n % i == 0 || n % (i + 2) == 0)
-			return false; 
-	}
+    return count;
+}
 
-	return true;   
+
+//--------OPTIMIZED APPROACH-----
+//T.C = O(root(n))
+bool prime_optimized(int num)
+{
+    int count = 0;
+
+    if(num==0 || num==1)                // edge case 1
+        return false;
+
+    if(num==2 || num==3)                    //edge case 2
+        return true;
+
+    if(num%2==0 || num%3==0)                    //edge case 3
+        return false;
+
+    for(int i=5; i*i<=num ; i=i+6)                    //there always exist a pair(x,y) in two halves of a numbers "n", such that n%x==0 and n%y==0
+    {
+        if(num%i==0 || num%(i+2)==0)
+            return false;
+    }
 }
 
 int main()
 {
-    int num, count=2;
-    cout<<"0 = False, 1 = True";
-    cout<<"\nEnter your number: ";
+    int num;
+    cout<<"Enter the number: ";
     cin>>num;
-    cout<<"Is "<<num<<" a prime number: "<<Bool(num);
-    return 0;
+
+    if(prime_optimized(num)==true)
+       cout<<num<<" is a prime number";
+    else
+        cout<<num<<" is not a prime number";
+
 }
-
-/*
-Naive Approach
-Time Complexity: O(n-2) = O(n)
-  for(int i=2 ; i<num ; i++)
-    {
-        if(num%i==0)
-        {
-            cout<<"Not a Prime Number";
-            count++;
-            break;
-        }
-    }
-
-    if(num==0 || num==1)
-        cout<<"Neither prime nor composite";
-    
-    else if(count==2)
-        cout<<"Prime Number";
-
-Optimal 1
-
-if(num==1 || num==0)
-        return false;
-    for(int i=2 ; i<num ; i++)
-    {
-        if(num%i==0)
-            return false;
-    }
-    return true;  
-
-*/
