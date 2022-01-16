f1d2fd273fb1e6693e2ba8921d2d9faf0c3d785f
Akanksha Pandaw <akankshapandaw8@gmail.com>
2022-01-16 22:17:29 +0530

    Checking Kth bit is SET or not

---
+++ Bit_Masking/checkBit.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+bool checkBit_leftShift (int num, int k)
+{
+    if(num & (1<<(k-1)) !=0)     // using left shift
+        return true;
+    else
+        return false;
+}
+
+bool checkBit_rightShift (int num, int k)
+{
+    if( (num>>(k-1)) & 1 !=0)     // using left shift
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int num,k;
+    cout<<"Enter your binary number: ";
+    cin>>num;
+
+    cout<<"Enter the position of bit to check: ";
+    cin>>k;
+
+    cout<<"The bit at position "<<k<<" is "<<((checkBit_rightShift(num,k)) ? "SET" : "NOT SET");
+
+}
