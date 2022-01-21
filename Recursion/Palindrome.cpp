35994217eb0373e4854a56b0533fb0968cc94d93
Akanksha Pandaw <akankshapandaw8@gmail.com>
2022-01-22 00:06:35 +0530

    Checking for palindrome strings

---
+++ Recursion/Palindrome.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+bool palindrome(string &str, int start, int end)
+{
+    if(start>=end)
+        return true;
+    return( (str[start]==str[end]) && (palindrome(str,start+1,end-1)));
+}
+
+int main()
+{
+    string str;
+    cout<<"Enter the string: ";
+    cin>>str;
+    cout<<"The given is "<<(palindrome(str,0,str.length()-1) ? "a Palindrome" : "Not a Palindrome");
+}
