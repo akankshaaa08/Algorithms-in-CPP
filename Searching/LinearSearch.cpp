dd8ffb8537795a73fd04f968bc5e72e6ac678e02
Akanksha Pandaw <akankshapandaw8@gmail.com>
2022-02-10 11:08:54 +0530

    Linear Search

---
+++ Searching/LinearSearch.cpp
@@ -0,0 +1,39 @@
+//T.C ==> O(log n)
+
+#include<iostream>
+using namespace std;
+
+int LSearch(int ar[], int num, int key)
+{
+    for(int i=0 ; i<num ; i++)
+    {
+        if(key == ar[i])
+            return i;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int n , key;
+    
+    cout<<"Enter the number of values in the array: ";
+    cin>>n;
+    
+    cout<<"Enter "<<n<<" values in the array: ";
+    int ar[n];
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>ar[i];
+    }
+
+    cout<<"Enter the number to be found: ";
+    cin>>key;
+
+    int result = LSearch(ar, n, key);
+
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+}
